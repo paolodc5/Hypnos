6e312f011a4dc90178efaa8a4612d73d69fb4609
Paolo Di Ciano <135977768+paolodc5@users.noreply.github.com>
2025-05-19 14:38:46 +0200

    Fixing conflicts in merge Now both doctor and patient gui work

--- gui/mainpat.py
+++ gui/mainpat.py
@@ -13,8 +13,17 @@ def start_gui():
 if __name__ == "__main__":
     # start_gui()
     # Create a dummy or real Patient object as needed
-    patient = Patient(patient_id=102, name="Maria", surname="Rossi", email="maria@hypnos.com", password="1234", birth_date="1995-07-05", 
-                      age=29, gender="F", phone_number="0987654321", fiscal_code="MRARSS95", doctor_id=1)
+    patient = Patient(
+        patient_id=102,
+        name="Maria",
+        surname="Rossi",
+        birth_date="1995-07-05",
+        age=29,
+        gender="F",
+        phone_number="0987654321",
+        fiscal_code="MRARSS95",
+        doctor_id=1
+    )
 
     app = PatientApp(patient)
     app.geometry("1200x700")
